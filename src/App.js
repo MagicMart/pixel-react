3a4f2d181fda4ef3db966afefc4bce89a6c6f046
Martin Tudor <martcan27@gmail.com>
2018-10-11 19:58:19 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Cell from "./components/Cell";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        const arr1 = [];
+        for (let i = 0; i < 100; i += 1) {
+            arr1.push(<Cell />);
+        }
+        return (
+            <div className="App">
+                <div className="container">
+                    <h1>Pixel Art Maker</h1>
+                    {arr1}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Cell.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Cell extends Component {
+    state = { background: "white" };
+    changeColor = () => {
+        this.setState(function(state) {
+            return state.background === "red"
+                ? { background: "white" }
+                : { background: "red" };
+        });
+    };
+    render() {
+        return (
+            <div
+                className="cell"
+                style={{
+                    background: this.state.background,
+                    border:
+                        this.state.background === "white"
+                            ? "black solid 0.5px"
+                            : `${this.state.background} solid 0.5px`
+                }}
+                onClick={() => this.changeColor()}
+            />
+        );
+    }
+}
+
+export default Cell;
