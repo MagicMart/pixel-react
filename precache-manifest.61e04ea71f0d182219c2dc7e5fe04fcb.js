ea2e0af2906d36683d071b9bcdd03614d1b05c78
Martin Tudor <martcan27@gmail.com>
2018-10-20 19:20:37 +0100

    Updates

---
+++ precache-manifest.61e04ea71f0d182219c2dc7e5fe04fcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d3fb7b5af09095d46ae",
+    "url": "/static/js/main.9d3fb7b5.chunk.js"
+  },
+  {
+    "revision": "26e332c58013aaf33d9a",
+    "url": "/static/js/1.26e332c5.chunk.js"
+  },
+  {
+    "revision": "9d3fb7b5af09095d46ae",
+    "url": "/static/css/main.9d7d2508.chunk.css"
+  },
+  {
+    "revision": "bb2a4adde4ef0923ea28dc759e1bd8da",
+    "url": "/index.html"
+  }
+];
